bbdda60196518d3c8e4392c3cbb0186d7b0a2eda
td041 <trinhtrantrungduc@gmail.com>
2025-09-06 21:27:52 +0700

    feat(login): setup tanstack query and code login logic

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
+import AppProvider from "../components/app-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -21,21 +22,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body suppressHydrationWarning={true}
+      <body
+        suppressHydrationWarning={true}
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-          <Toaster />
-        </ThemeProvider>
+        <AppProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </AppProvider>
       </body>
     </html>
   );
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,5 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
+import { UseFormSetError } from "react-hook-form";
 import { twMerge } from "tailwind-merge";
+import { EntityError } from "./http";
+import { toast } from "@/components/ui/use-toast";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -8,3 +11,29 @@ export function cn(...inputs: ClassValue[]) {
 export function normalizePath(path: string) {
   return path.startsWith("/") ? path.slice(1) : path;
 }
+
+export const handleErrorApi = ({
+  error,
+  setError,
+  duration,
+}: {
+  error: any;
+  setError?: UseFormSetError<any>;
+  duration?: number;
+}) => {
+  if (error instanceof EntityError && setError) {
+    error.payload.errors.forEach((item) => {
+      setError(item.field, {
+        type: "server",
+        message: item.message,
+      });
+    });
+  } else {
+    toast({
+      title: "Lỗi",
+      description: error?.payload?.message ?? "Lỗi không xác định",
+      variant: "destructive",
+      duration: duration ?? 5000,
+    });
+  }
+};
